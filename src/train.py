e3ceed9c756d1e98a44c5ea08a4b0afef35f7c1e
Favck <nikitagarkusha2006@gmail.com>
2026-06-24 10:34:07 +0300

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,53 @@
+from tqdm.notebook import tqdm
+from src.dataset import MyDataset
+import seaborn as sns
+import torch
+
+
+
+
+criterion = DiceLoss()
+epochs = 40
+avg_train_loss = []
+avg_val_dice = []
+avg_val_iou = []
+
+for epoch in tqdm(range(epochs)):
+  model.train()
+  train_loss = 0
+
+  for X_batch, Y_batch in tqdm(train_loader):
+    X_batch = X_batch.to(device)
+    Y_batch = Y_batch.to(device)
+    Y_pred = model(X_batch)
+    loss = criterion(Y_pred, Y_batch)
+    train_loss += loss.item()
+
+    optim.zero_grad()
+
+    loss.backward()
+    optim.step()
+
+  avg_train_loss.append(train_loss/len(train_loader))
+
+  model.eval()
+  val_loss_dice = 0
+  val_loss_iou = 0
+  with torch.no_grad():
+    for X_val, Y_val in val_loader:
+      X_val = X_val.to(device)
+      Y_val = Y_val.to(device)
+
+      Y_pred = model(X_val)
+
+      loss = criterion(Y_pred, Y_val)
+      val_loss_dice += loss.item()
+
+      val_loss_iou += jacard(torch.sigmoid(Y_pred), Y_val).to(device).item()
+
+
+
+  avg_val_dice.append(val_loss_dice/len(val_loader))
+  avg_val_iou.append(val_loss_iou/len(val_loader))
+  print(f"Epoch:{epoch} | Train: {avg_train_loss[-1] :.6f} | Val: {avg_val_dice[-1]:.6f} | Val: {avg_val_iou[-1]:.6f}")
+
